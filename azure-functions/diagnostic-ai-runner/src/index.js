5863e206e4825adab97a852feed7d8357ea30602
jmerrillorg <93427404+jmerrillorg@users.noreply.github.com>
2026-07-05 05:35:19 -0400

    Fix diagnostic runner function entrypoint
    Use a single Node v4 entrypoint so Azure Functions discovers all HTTP registrations.

---
+++ azure-functions/diagnostic-ai-runner/src/index.js
@@ -0,0 +1,15 @@
+"use strict";
+
+require("./functions/getPublisherRecommendationReview");
+require("./functions/runAgreementPackageSend");
+require("./functions/runEditorialReviewNow");
+require("./functions/runIntentionalLeaderAuthorResponse");
+require("./functions/runMilestone6ContinuationCommunication");
+require("./functions/runMilestone6EvidenceRecoveryLog");
+require("./functions/runMilestone6OpportunityUpdate");
+require("./functions/runMilestone6PaymentOptionCapture");
+require("./functions/runPreContractEditorialReview");
+require("./functions/runPublisherRecommendationAction");
+require("./functions/runPublisherReviewDecision");
+require("./functions/runPublishingMailboxReplyCheck");
+require("./functions/runStage0Diagnostic");
